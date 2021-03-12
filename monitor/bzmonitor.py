d65864c2cab814b1744b9ad8f13842915acd56a3
Feller <iamliurenjie@gmail.com>
2021-03-13 00:44:59 +0800

    Update bzmonitor.py

--- monitor/bzmonitor.py
+++ monitor/bzmonitor.py
@@ -11,17 +11,17 @@ class bzMonitor():
 		self.reqHeaders = {
 			'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:54.0) Gecko/20100101 Firefox/54.0',
 			'Content-Type': 'application/x-www-form-urlencoded',
-			'Referer': 'https://space.bilibili.com/3345720/video',
+			'Referer': 'https://space.bilibili.com/',
 			'Connection': 'close',
 			'Accept-Language': 'zh-CN,zh;q=0.8,en-US;q=0.5,en;q=0.3'
 		}
-		self.uid = ['3345720']#这里添加关注人的uid
+		self.uid = ['11442277','386234695','483311105','4401694','388657573','319076','7887262','125526','430726','25910292','25876945','149558293','946974']
 	#获取各用户的json连接
 	def getbzurl(self):
 		try:
 			self.bzurl = []
 			for i in self.uid:
-				url = 'https://api.bilibili.com/x/space/navnum?mid=%s&jsonp=jsonp&callback='%(i)
+				url = 'https://api.bilibili.com/x/space/arc/search?mid=%s'%(i)
 				self.bzurl.append(url)
 		except Exception as e:
 			self.echoMsg('Error',e)
@@ -31,7 +31,7 @@ class bzMonitor():
 		try:
 			for i in self.bzurl:
 				res = requests.get(i,headers=self.reqHeaders)
-				num = res.json()['data']['video']
+				num = res.json()['data']['page']['count']
 				with open('bilibili.txt','a') as f:
 					f.write(i+':'+str(num)+'\n')
 				self.echoMsg('Info','视频数目获取成功')
@@ -49,12 +49,12 @@ class bzMonitor():
 					bilibili.append(line)
 			for i in self.bzurl:
 				res = requests.get(i,headers=self.reqHeaders)
-				num = res.json()['data']['video']
+				num = res.json()['data']['page']['count']
 				url2 = i+':'+str(num)
 				if url2 not in bilibili:
 					with open('bilibili.txt','a') as f:
 						f.write(url2+'\n')
-					self.echoMsg('Info','B站视频更新啦!!!')
+					self.echoMsg('Info','B站视频更新!')
 					uid = re.findall('\\d+', i)[0]
 					req = requests.get('https://space.bilibili.com/'+str(uid),headers=self.reqHeaders)
 					nickName = re.findall('<title>(.*?)的个人空间', req.text)[0]
