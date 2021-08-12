a721e08e66ccef86ce8ee28beb787110ed059d39
Feller <iamliurenjie@gmail.com>
2021-08-12 17:40:37 +0800

    Update content.py

--- content.py
+++ content.py
@@ -6,8 +6,9 @@
 import time
 import datetime
 
-import requests,json,sys
+import requests, json, sys
 import re
+
 
 def trans_format(time_string, from_format, to_format='%Y.%m.%d %H:%M:%S'):
     """
@@ -17,13 +18,14 @@ def trans_format(time_string, from_format, to_format='%Y.%m.%d %H:%M:%S'):
     :param to_format:
     :return:
     """
-    time_struct = time.strptime(time_string,from_format)
+    time_struct = time.strptime(time_string, from_format)
     times = time.strftime(to_format, time_struct)
     return times
 
-def wbcontent(txt,createtime,sourcel,fasname,deit,reposts,attitudes,comments,picnum):
-    AAA=txt['mblog']['text']
-    span=re.sub('<span(.*?)</span>','',AAA)
+
+def wbcontent(txt, createtime, sourcel, fasname, deit, reposts, attitudes, comments, picnum,idd):
+    AAA = txt['mblog']['text']
+    span = re.sub('<span(.*?)</span>', '', AAA)
     atab = re.sub('<a(.*?)</a>', '', span)
     img = re.sub('<img alt(.*?)/>', '', atab)
     bra = re.sub('<br />', '', img)
@@ -32,11 +34,10 @@ def wbcontent(txt,createtime,sourcel,fasname,deit,reposts,attitudes,comments,pic
     # print(format_time)
     # print(sourcel)
     # print(fasname)
-    if deit ==True:
-        deit='已编辑'
+    if deit == True:
+        deit = '已编辑'
     else:
-        deit=''
-
+        deit = ''
 
     # print(deit)
     #
@@ -44,17 +45,15 @@ def wbcontent(txt,createtime,sourcel,fasname,deit,reposts,attitudes,comments,pic
     # print(comments)
     # print(attitudes)
     # print(picnum)
-    #
-
-
+    #https://m.weibo.cn/status/4669358909428804
+    detalurl="https://m.weibo.cn/status/"+idd
 
     # print(type(deit))
 
-    reposts2=str(reposts)
+    reposts2 = str(reposts)
     comments2 = str(comments)
     attitudes2 = str(attitudes)
     picnum2 = str(picnum)
-
 
     # print(type(reposts2))
     # print(type(comments2))
@@ -74,16 +73,13 @@ def wbcontent(txt,createtime,sourcel,fasname,deit,reposts,attitudes,comments,pic
     delta = str(delta)
     timedelay = delta
 
-    imgpost='https://push.bot.qw360.cn/send/e54011f0-f9aa-11eb-806f-9354f453c154'
+    imgpost = 'https://push.bot.qw360.cn/send/e54011f0-f9aa-11eb-806f-9354f453c154'
     headers = {'Content-Type': 'application/json'}
-    fasongneir='@'+fasname+'\n'+format_time+' '+'来自 '+sourcel+' '+'\n'+'✔'+picnum2+'张图'+' '+'\n'+'✔'+deit+reposts2+'转'+' '+attitudes2+'赞'+' '+comments2+'评'+' '+'\n'+'✔'+'推送时间：'+tzshj+' '+'\n'+'✔'+'延时推送：'+timedelay+'分钟'+' '+'\n'+'\n'+'◕‿-｡　｡◕‿◕'+'\n'+bra+'\n'+'◕‿-｡　｡◕‿◕'
+    fasongneir = '@' + fasname + '\n' + format_time + ' ' + '来自 ' + sourcel + ' ' + '\n' + '✔' + picnum2 + '张图' + ' ' + '\n' + '✔' + deit + reposts2 + '转' + ' ' + attitudes2 + '赞' + ' ' + comments2 + '评' + ' ' + '\n' + '✔' + '推送时间：' + tzshj + ' ' + '\n' + '✔' + '延时推送：' + timedelay + '分钟' + ' '+'\n'+ '✔' +'原博链接：'+detalurl+ ' ' + '\n' + '\n' + '◕‿-｡　｡◕‿◕' + '\n' + bra + '\n' + '◕‿-｡　｡◕‿◕'
     print(fasongneir)
-    postdata=json.dumps({"msg":fasongneir})
+    postdata = json.dumps({"msg": fasongneir})
     time.sleep(4)
-    repp=requests.post(url=imgpost, data=postdata, headers=headers)
-
-
-
+    repp = requests.post(url=imgpost, data=postdata, headers=headers)
 
 #
 # if __name__ == '__main__':
