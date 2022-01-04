e8a3256a24a1e60bb116c6ee9a0d5d175c50ce27
Feller <iamliurenjie@gmail.com>
2022-01-04 11:59:24 +0800

    Update index.py

--- index.py
+++ index.py
@@ -21,30 +21,30 @@ def trans_format(time_string, from_format, to_format='%Y.%m.%d %H:%M:%S'):
     return times
 
 #推送函数
-def notify(text):
-    flag = True
-    try:
-        qs = urllib.parse.urlencode(
-            dict(
-                #一键免费推送信息到手机https://sre24.com/
-                token="e874c767e46379511d9d566909b2ce92",  #token对应微信
-                msg=text,
-            ))
-        rs = requests.get(url="https://sre24.com/api/v1/push?" + qs).json()
-        assert int(rs["code"] / 100) == 2, rs
-    except Exception as e:
-        print(e)
-        flag = False
-    return flag
-
-def wbweixin(dicts):
-    
-    created_at1=dicts['created_at']
-    format_time = trans_format(created_at1, '%a %b %d %H:%M:%S +0800 %Y', '%Y-%m-%d %H:%M:%S')
-    # dicts['created_at']
-    text = "" + "@"+ dicts['nickName'] + "发微博了\n" + "发送时间: " + format_time + "\n" + dicts['text']
-    flag = notify(text)
-    return flag
+# def notify(text):
+#     flag = True
+#     try:
+#         qs = urllib.parse.urlencode(
+#             dict(
+#                 #一键免费推送信息到手机https://sre24.com/
+#                 token="e874c767e46379511d9d566909b2ce92",  #token对应微信
+#                 msg=text,
+#             ))
+#         rs = requests.get(url="https://sre24.com/api/v1/push?" + qs).json()
+#         assert int(rs["code"] / 100) == 2, rs
+#     except Exception as e:
+#         print(e)
+#         flag = False
+#     return flag
+#
+# def wbweixin(dicts):
+#
+#     created_at1=dicts['created_at']
+#     format_time = trans_format(created_at1, '%a %b %d %H:%M:%S +0800 %Y', '%Y-%m-%d %H:%M:%S')
+#     # dicts['created_at']
+#     text = "" + "@"+ dicts['nickName'] + "发微博了\n" + "发送时间: " + format_time + "\n" + dicts['text']
+#     flag = notify(text)
+#     return flag
 
 
 # def bzweixin(dicts):
@@ -71,7 +71,7 @@ def main(*args):
             w.getWBQueue()
     newWB = w.startmonitor()
     if newWB is not None:
-        print(wbweixin(newWB))  #推送成功则输出True
+        print("wbweixin(newWB)")  #推送成功则输出True
     # #B站部分
     # b = bzmonitor.bzMonitor()
     # b.getbzurl()
